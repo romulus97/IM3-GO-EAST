998ec7dc5ce089866f6b80ad965e9cfc04017c8f
Jordan Kern <jkern@ncsu.edu>
2022-07-25 21:50:46 -0400

    Update reduced_network_data_allocation.py

--- reduced_network_data_allocation.py
+++ reduced_network_data_allocation.py
@@ -557,22 +557,52 @@ for NN in NODE_NUMBER:
             df_gens_heat_rate['NewBusNum'] = NB_hr
     
             names = list(df_gens['BusName'])
+            fts = list(df_gens['FuelType'])
             names_hr = list(df_gens_heat_rate['BusName'])
             
             # remove numbers and spaces
             for n in names:
                 i = names.index(n)
                 corrected = re.sub(r'[^A-Z]',r'',n)
+                f = fts[i]
+                if f == 'NUC (Nuclear)':
+                    f = 'Nuc'
+                elif f == 'NG (Natural Gas)':
+                    f = 'NG'
+                elif f == 'BIT (Bituminous Coal)':
+                    f = 'NG'
+                elif f == 'SUN (Solar)':
+                    f = 'S'
+                elif f == 'WAT (Water)':
+                    f = 'H'
+                elif f == 'WND (Wind)':
+                    f = 'W'
+                    
+                corrected = corrected + '_' + f
                 names[i] = corrected
                 
             for n in names_hr:
                 i = names_hr.index(n)
                 corrected = re.sub(r'[^A-Z]',r'',n)
+                f = fts[i]
+                if f == 'NUC (Nuclear)':
+                    f = 'Nuc'
+                elif f == 'NG (Natural Gas)':
+                    f = 'NG'
+                elif f == 'BIT (Bituminous Coal)':
+                    f = 'NG'
+                elif f == 'SUN (Solar)':
+                    f = 'S'
+                elif f == 'WAT (Water)':
+                    f = 'H'
+                elif f == 'WND (Wind)':
+                    f = 'W'
+                    
+                corrected = corrected + '_' + f
                 names_hr[i] = corrected
             
             df_gens['PlantNames'] = names
-            df_gens_heat_rate['PlantNames'] = names_hr
-            
+            df_gens_heat_rate['PlantNames'] = names_hr            
             NB = df_gens['NewBusNum'].unique()
             plants = []
             caps = []
